dcad2b04863f2f4d7c3e50ed4531c820c7bf0649
wnzczj <1664706850@qq.com>
2021-08-21 21:48:49 +0800

    add first

--- src/main/java/com/group3/fundmgt/manager/ManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group3.fundmgt.manager;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManagerRepository extends JpaRepository<Manager, Long> {
-
-}
