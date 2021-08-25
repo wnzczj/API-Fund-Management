e6aac08c47a83a91595c9d15272fddc2c8edabe2
wnzczj <1664706850@qq.com>
2021-08-25 14:39:57 +0800

    correct property

--- src/main/java/com/group3/fundmgt/FundmgtApplication.java
+++ src/main/java/com/group3/fundmgt/FundmgtApplication.java
@@ -38,8 +38,8 @@ public class FundmgtApplication {
             List<Manager> managers = List.of(
                     new Manager("Chris", "Gardner", new ArrayList<>()),
                     new Manager("Frank", "Abagnale", new ArrayList<>()),
-                    new Manager("Bernard ", "Madoff", new ArrayList<>()),
-                    new Manager("Donald ", "Trump", new ArrayList<>())
+                    new Manager("Bernard ", "Madoff", new ArrayList<>())
+                    // new Manager("Donald ", "Trump", new ArrayList<>())
             );
             managerRepository.saveAll(managers);
 
